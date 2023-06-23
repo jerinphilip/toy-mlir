f020627376fbe3e5fdca7e81090228de53b4eaa6
Jerin Philip <jerinphilip@live.in>
2023-06-23 19:11:55 +0530

    Export compile commands so clangd assistance behave better

--- scripts/standalone-build.sh
+++ scripts/standalone-build.sh
@@ -3,9 +3,9 @@
 MLIR_DIR=/home/jerin/code/llvm-project/build/lib/cmake/mlir
 LLVM_EXTERNAL_LIT=/home/jerin/code/llvm-project/build/bin/llvm-lit
 
-cmake -G Ninja .. \
+cmake -G Ninja -B build -S ./ \
   -DMLIR_DIR=$MLIR_DIR -DLLVM_EXTERNAL_LIT=$LLVM_EXTERNAL_LIT \
-  -DLLVM_USE_SANITIZER="Address;Undefined"
+  -DLLVM_USE_SANITIZER="Address;Undefined" \
+  -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 
 cmake --build . --target toyc
-
