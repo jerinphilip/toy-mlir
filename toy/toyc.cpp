e565bb3aaabb1689890b2555e40e04513868ca59
Jerin Philip <jerinphilip@live.in>
2023-11-07 04:11:40 +0530

    Break follow to see what's going on

--- toy/toyc.cpp
+++ toy/toyc.cpp
@@ -17,6 +17,7 @@
 #include "toy/Lexer.h"
 #include "toy/MLIRGen.h"
 #include "toy/Parser.h"
+#include "toy/Passes.h"
 
 #include "mlir/IR/AsmState.h"
 #include "mlir/IR/BuiltinOps.h"
@@ -125,8 +126,16 @@ int dumpMLIR() {
     if (mlir::failed(mlir::applyPassManagerCLOptions(pm)))
       return 4;
 
-    // Add a run of the canonicalizer to optimize the mlir module.
-    pm.addNestedPass<mlir::toy::FuncOp>(mlir::createCanonicalizerPass());
+    // Inline all functions into main and then delete them.
+    pm.addPass(mlir::createInlinerPass());
+
+    // Now that there is only one function, we can infer the shapes of each of
+    // the operations.
+    mlir::OpPassManager &optPM = pm.nest<mlir::toy::FuncOp>();
+    optPM.addPass(mlir::toy::createShapeInferencePass());
+    optPM.addPass(mlir::createCanonicalizerPass());
+    optPM.addPass(mlir::createCSEPass());
+
     if (mlir::failed(pm.run(*module)))
       return 4;
   }
